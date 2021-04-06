46f6274e3eb28a1728e15633ea92051f40531374
dolonzo <49281109+dolonzo@users.noreply.github.com>
2021-04-06 09:49:43 -0400

    Added GUI window

--- eeg_checkburst.m
+++ eeg_checkburst.m
@@ -22,41 +22,57 @@
 
 function [EEG, isburst] = eeg_checkburst(EEG, pulse_event)
 
-fprintf('Checking for burst patterning...\n');
-
-pulse_indices = find(ismember({EEG.event(:).type}, pulse_event));
-pulse_latencies = [EEG.event(pulse_indices).latency];
-
-pulse_lat_diffs = nan(1, length(pulse_latencies));
-for i = 2:length(pulse_latencies)
-    pulse_lat_diffs(i) = pulse_latencies(i) - pulse_latencies(i-1);
+if nargin < 1
+    help eeg_checkburst
+    return
 end
 
-burst_cutoff = 45e-3*EEG.srate; %burst_cutoff set to 45 ms to account for jitter in pulse frequency
-bool_burst_latency = pulse_lat_diffs<burst_cutoff;
-
-if ~any(bool_burst_latency)
-    isburst = 0;
-    return;
+if nargin < 2
+    geometry = {[1, 0.5]};
+    uilist = {...
+        {'Style', 'text', 'string', 'Event marking TMS pulses'}...
+        {'Style', 'edit', 'string', '' 'tag' 'event'} };
+    [~, ~, ~, outstruct, ~] = inputgui('geometry', geometry, 'uilist', uilist, 'title', 'Check for burst pattern stimulation--eeg_checkburst()');
+    [EEG, isburst] = eeg_checkburst(EEG, split(outstruct.event));
+    
+else
+    
+    fprintf('Checking for burst patterning...\n');
+    
+    pulse_indices = find(ismember({EEG.event(:).type}, pulse_event));
+    pulse_latencies = [EEG.event(pulse_indices).latency];
+    
+    pulse_lat_diffs = nan(1, length(pulse_latencies));
+    for i = 2:length(pulse_latencies)
+        pulse_lat_diffs(i) = pulse_latencies(i) - pulse_latencies(i-1);
+    end
+    
+    burst_cutoff = 45e-3*EEG.srate; %burst_cutoff set to 45 ms to account for jitter in pulse frequency
+    bool_burst_latency = pulse_lat_diffs<burst_cutoff;
+    
+    if ~any(bool_burst_latency)
+        isburst = 0;
+        return;
+    end
+    
+    isburst = 1;
+    
+    firsts = find(~bool_burst_latency);
+    burst_count = zeros(1, length(firsts)-1);
+    for i = 1:length(firsts)-1
+        burst_count(i) = firsts(i+1)-firsts(i);
+    end
+    
+    if any(burst_count~=burst_count(1))
+        warning('Warning: burst counts not consistent. Check for errant pulses in the dataset.');
+    end
+    
+    for ind = 1:length(firsts)
+        EEG.event(end+1) = EEG.event(pulse_indices(firsts(ind)));
+        EEG.event(end).type = 'First';
+    end
+    
 end
-
-isburst = 1;
-
-firsts = find(~bool_burst_latency);
-burst_count = zeros(1, length(firsts)-1);
-for i = 1:length(firsts)-1
-    burst_count(i) = firsts(i+1)-firsts(i);
-end
-
-if any(burst_count~=burst_count(1))
-    warning('Warning: burst counts not consistent. Check for errant pulses in the dataset.');
-end
-
-for ind = 1:length(firsts)
-    EEG.event(end+1) = EEG.event(pulse_indices(firsts(ind)));
-    EEG.event(end).type = 'First';
-end
-
 
 end
 
